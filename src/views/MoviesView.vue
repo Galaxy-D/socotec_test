bdbfad697c51539c5ad21df8c60e2c210d4c7395
Amine BENLAMA <benlama.amine@gmail.com>
2023-01-30 11:00:53 +0100

    implémentation des views

---
+++ src/views/MoviesView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="home">
+      <MovieList/>
+    </div>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  import MovieList from '@/components/movieList.vue'
+  
+  export default {
+    name: 'MoviesView',
+    components: {
+      MovieList
+    },
+    data () {
+        return {
+          page: 1,
+        }
+      },
+  }
+  </script>
+  
